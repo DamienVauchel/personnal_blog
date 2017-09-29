2ad24e73933877c5305b29eb6368f610527373c9
DamienVauchel <damien.vauchel@gmail.com>
2017-09-29 16:27:26 +0200

    blog.php modifications + blog_post.php creation and customisation

--- blog.php
+++ blog.php
@@ -16,19 +16,23 @@
     <!--############################################################################
      ### MODELE POUR UN ARTICLE A ENTRER DANS UNE BOUCLE POUR TOUS LES AFFICHER ####
      ###############################################################################-->
-    <div class="row thumbnail">
-        <br><br>
-        <div class="col-md-6">
-            <img src="assets/img/p03.png" alt="" style="max-width: 100%;">
-        </div><!-- col-lg-6 -->
-        <div class="col-md-6">
-            <h2 class="text-center"><b>TITRE DU BLOG POST</b></h2>
-            <div class="text-right"><small style="text-decoration: underline;"><i class="fa fa-clock-o" aria-hidden="true"></i> Date de dernière mise à jour: 29/09/2017</small></div>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-            <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            <p class="text-right"><a href="#">... Lire l'article</a></p>
-        </div>
-    </div><!-- row -->
+    <a href="blog_post.php">
+        <div class="row thumbnail">
+            <br><br>
+            <div class="col-md-6">
+                <img src="assets/img/p03.png" alt="" style="max-width: 100%;">
+            </div><!-- col-lg-6 -->
+            <div class="col-md-6">
+                <h2 class="text-center"><b>TITRE DU BLOG POST</b></h2>
+                <div class="text-right"><small style="text-decoration: underline;"><i class="fa fa-clock-o" aria-hidden="true"></i> Date de dernière mise à jour: 29/09/2017</small></div>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <p class="text-right"><a href="#">... Lire l'article</a></p>
+            </div>
+            <br><br>
+        </div><!-- row -->
+    </a>
+
     <hr>
 <!--    FIN MODELE-->
 </div>
